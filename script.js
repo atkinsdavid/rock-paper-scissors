ab70a16d49bdd0358ec9d712b65e46f161ccfcb0
David Atkins <david.atkins@me.com>
2022-07-16 15:24:06 -0500

    added basic UI and score tracker

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const a = 'rock',b = 'paper',c= 'scissors';
-// let comp = 0;
-// let player = 0;
+const buttons = document.querySelectorAll('button');
+const round = document.getElementById('round');
+const player = document.getElementById('player');
+const computer = document.getElementById('computer');
+const winner = document.getElementById('winner');
+let compScore = 0;
+let playScore = 0;
 
 function computerPlay(){
     const options = [a,b,c]
@@ -8,58 +13,44 @@ function computerPlay(){
     return options[i];
 }
 
-// function playRound(){
 function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
-        console.log("Draw");
-    } else if (playerSelection === a && computerSelection === b) {
-        console.log(`You lose, ${b} covers ${a}.`)
-    } else if (playerSelection === a && computerSelection === c) {
-        console.log(`You win, ${a} crushes ${c}!`)
-    } else if (playerSelection === b && computerSelection === c) {
-        console.log(`You lose, ${c} cut ${b}.`)
-    } else if (playerSelection === b && computerSelection === a) {
-        console.log(`You win, ${b} covers ${a}!`)
-    } else if (playerSelection === c && computerSelection === a) {
-        console.log(`You lose, ${a} crushes ${c}.`)
-    } else if (playerSelection === c && computerSelection === b) {
-        console.log(`You win, ${c} cut ${b}!`)
+    if (playScore ===5) {
+        winner.textContent = "You win!!!";
+    } else if (compScore === 5) {
+        winner.textContent = "You lose!!!";
     } else {
-        console.log("Something has gone awry...");
-    }
-}
-
-
-// After changing to passing parameters
-// var playerSelection = a;
-// console.log(playRound(playerSelection, computerSelection));
-// playerSelection = b;
-// console.log(playRound(playerSelection, computerSelection));
-// playerSelection = c;
-// console.log(playRound(playerSelection, computerSelection));
-
-// How I did it with global variables
-// player = a;
-// console.log(`Player plays ${player}`);
-// computerPlay();
-// console.log(`Computer plays ${comp}`);
-// compare();
-// player = b;
-// console.log(`Player plays ${player}`);
-// computerPlay();
-// console.log(`Computer plays ${comp}`);
-// compare();
-// player = c;
-// console.log(`Player plays ${player}`);
-// computerPlay();
-// console.log(`Computer plays ${comp}`);
-// compare();
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Enter either rock, paper, or scissors");
-        const computerSelection = computerPlay();
-        playRound(playerSelection,computerSelection);
+        if (playerSelection === computerSelection){
+            round.textContent = 'Draw';
+        } else if (playerSelection === a && computerSelection === b) {
+            round.textContent = `You lose, ${b} covers ${a}.`;
+            compScore = ++compScore;
+        } else if (playerSelection === a && computerSelection === c) {
+            round.textContent = `You win, ${a} crushes ${c}!`;
+            playScore = ++playScore;
+        } else if (playerSelection === b && computerSelection === c) {
+            round.textContent = `You lose, ${c} cut ${b}.`;
+            compScore = ++compScore;
+        } else if (playerSelection === b && computerSelection === a) {
+            round.textContent = `You win, ${b} covers ${a}!`;
+            playScore = ++playScore;
+        } else if (playerSelection === c && computerSelection === a) {
+            round.textContent = `You lose, ${a} crushes ${c}.`;
+            compScore = ++compScore;
+        } else if (playerSelection === c && computerSelection === b) {
+            round.textContent = `You win, ${c} cut ${b}!`;
+            playScore = ++playScore;
+        } else {
+            round.textContent = "Something has gone awry...";
+        };
+        player.textContent = playScore;
+        computer.textContent = compScore;
     };
-}
-game();
+};
+
+function game(e) {
+    let playerSelection = this.id;
+    const computerSelection = computerPlay();
+    playRound(playerSelection,computerSelection);
+};
+
+buttons.forEach(button => button.addEventListener('click', game));
